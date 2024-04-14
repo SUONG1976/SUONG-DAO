42554568f369640ff2dc60d2d2ce514f5fc671f9
SUONG1976 <166966204+SUONG1976@users.noreply.github.com>
2024-04-14 09:35:08 +0700

    SUONG DAO

---
+++ data/slide1.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{486108E1-7A0A-43D9-9050-6DE2DDC607C3}'];loadHandler&&loadHandler(0, '<div id="spr0_5255578f"><div id="spr1_5255578f" class="kern slide"><img id="img0_5255578f" src="data/img0.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_5255578f" class="kern slide"></div></div>', '{"s":[]}');})();
